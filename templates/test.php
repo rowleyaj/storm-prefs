764f1ecc5e66e6403be2e02a128e19d845fc5f88
Alex Rowley <rowleyaj@gmail.com>
2013-08-25 21:16:27 +0100

    Add all files again

---
+++ templates/test.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Alex
+ * Date: 18/08/2013
+ * Time: 20:35
+
+*/
+
+/**
+ * Test for
+ */
+public
+function test()
+{
+
+}
+
+/**
+ * Test for what are you testing
+ */
+public function testWhat are you testing()
+{
+
+}
+
+
+
+/**
+ * Sets up the fixture, for example, open a network connection.
+ * This method is called before a test is executed.
+ *
+ */
+protected function setUp()
+{
+    parent::setUp();
+    dfsf
+}
+
+/**
+ * Tears down the fixture, for example, close a network connection.
+ * This method is called after a test is executed.
+ */
+protected function tearDown()
+{
+
+    parent::tearDown();
+}
+
+
+/**
+ * This method is called after the last test of this test class is run.
+ *
+ * @since Method available since Release 3.4.0
+ */
+public static function tearDownAfterClass()
+{
+
+}
+
+/**
+ * Test for ThatthE
+ *
+ * @expectedException expected exception class
+ */
+public function testThatthE()
+{
+
+}
+
+/**
+ * Test for what are you testing
+ */
+public function testWhat are you testing()
+{
+
+}
